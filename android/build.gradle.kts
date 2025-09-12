899738a37d1383eeb8244219abeb6e01dddd219a
MechaNeil <markneilteves12345@gmail.com>
2025-09-12 10:59:47 +0800

    chore: Update Java version to 17 in build.gradle.kts and suppress obsolete warnings

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,6 +17,11 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    
+    // Suppress obsolete Java version warnings
+    tasks.withType<JavaCompile> {
+        options.compilerArgs.add("-Xlint:-options")
+    }
 }
 
 tasks.register<Delete>("clean") {
